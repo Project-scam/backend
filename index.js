7437b5e9c2468be35ac8eafcd208b7e8fd6d5bf9
andrea <villari.andrea@libero.it>
2025-12-16 17:04:24 +0100

    Correzione ad index.js

--- index.js
+++ index.js
@@ -26,6 +26,10 @@ app.get("/utenti", async (req, res) => {
   }
 });
 
-app.listen(3000, () => console.log("Server su http://localhost:3000"));
-
-node.module.exports = app;
+if (process.env.NODE_ENV !== "production") {
+  const PORT = process.env.PORT || 3000;
+  app.listen(PORT, () => {
+    console.log("Server su http://localhost:3000");
+  });
+}
+module.exports = app;
