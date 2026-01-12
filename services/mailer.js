00a4b2b3f3a629e6d7468abb344fd4f33296e169
Project-Scam <pwscamits-2526@libero.it>
2026-01-12 13:12:31 +0100

    modifica porta per SMTP da 465 a 587

--- services/mailer.js
+++ services/mailer.js
@@ -12,12 +12,18 @@ const nodemailer = require("nodemailer");
 
 const transporter = nodemailer.createTransport({
     host: "smtp.libero.it",
-    port: 465,
-    secure: true,
+    port: 587,
+    secure: false, // true per 465, false per altre porte
     auth: {
         user: 'pwscamits-2526@libero.it',
         pass: process.env.LIBERO_PASS
-    }
+    },
+    tls: {
+        rejectUnauthorized: false // Accetta certificati self-signed
+    },
+    connectionTimeout: 10000, // 10 secondi
+    greetingTimeout: 10000,
+    socketTimeout: 10000
 });
 
 module.exports = transporter;
