b452dad9308c61a4986c8d9942d45a2bf1f6cace
CatalinG <catalin.groppo@outlook.com>
2026-01-23 13:59:12 +0100

    Aggiornamento link frontEnd v.2

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 const isProduction = process.env.NODE_ENV === 'production';
 
 const FRONTEND_URL = isProduction
-    ? "https://pwscam.vercel.app/"
+    ? "https://pwscam.vercel.app"
     : "http://localhost:5173";
 
 module.exports = {
